64be76c82b5fe1c979220d75b12c96e90c49e0ac
PauMB <paumb85@gmail.com>
2020-04-18 00:24:47 +0200

    se realiza login y logout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Link, useHistory } from "react-router-dom";
 import  { Navbar, Nav }  from "react-bootstrap";
+import { AppContext } from "./libs/contextLib";
+import { Auth } from "aws-amplify";
 import Routes from "./Routes";
 import './App.css';
 
 function App() {
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
+
+  const history = useHistory();
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+  
+  async function onLoad() {
+    try {
+      await Auth.currentSession();
+      userHasAuthenticated(true);
+    }
+    catch(e) {
+      if (e !== 'No current user') {
+        alert(e);
+      }
+    }
+  
+    setIsAuthenticating(false);
+  }
+
+  async function handleLogout() {
+    await Auth.signOut();
+  
+    userHasAuthenticated(false);
+  
+    history.push("/login");
+  }
+
   return (
+    !isAuthenticating &&
     <div className="App container">
       <Navbar className="position-nav">
         <Nav>
             <Link to="/">Home</Link>
         </Nav>
-        <Nav>
+        {
+          isAuthenticated ?
+          <Nav>
+            <Link to="#" onClick={handleLogout}>Logout</Link>
+          </Nav>
+          :
+          <Nav>
             <Link className="login-singup" to="/signup">Signup</Link>
             <Link className="login-singup" to="/login">Login</Link>
-        </Nav>
+          </Nav>
+        }
+        
       </Navbar>
 
-      <Routes />
+      <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
+        <Routes />
+      </AppContext.Provider>
     </div>
   );
 }
--- src/containers/login/Login.js
+++ src/containers/login/Login.js
@@ -1,23 +1,38 @@
 import React, { useState } from "react";
 import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
+import { Auth } from "aws-amplify";
+import { useAppContext } from "../../libs/contextLib";
+import { useHistory } from "react-router-dom";
 import "./Login.css";
 
 export default function Login() {
+
+  const { userHasAuthenticated } = useAppContext();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const history = useHistory();
 
   function validateForm() {
     return email.length > 0 && password.length > 0;
   }
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
+  
+    try {
+      await Auth.signIn(email, password);
+      userHasAuthenticated(true);
+      history.push("/");
+    } catch (e) {
+      alert(e.message);
+    }
   }
 
   return (
     <div className="Login">
       <form onSubmit={handleSubmit}>
-        <FormGroup controlId="email" bsSize="large">
+        <FormGroup controlId="email">
           <FormLabel>Email</FormLabel>
           <FormControl
             autoFocus
@@ -26,7 +41,7 @@ export default function Login() {
             onChange={e => setEmail(e.target.value)}
           />
         </FormGroup>
-        <FormGroup controlId="password" bsSize="large">
+        <FormGroup controlId="password">
           <FormLabel>Password</FormLabel>
           <FormControl
             value={password}
@@ -34,7 +49,7 @@ export default function Login() {
             type="password"
           />
         </FormGroup>
-        <Button block bsSize="large" disabled={!validateForm()} type="submit">
+        <Button block disabled={!validateForm()} type="submit">
           Login
         </Button>
       </form>
